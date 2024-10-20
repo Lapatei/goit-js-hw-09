8fe125e39e10e36ea3ef19dd5652fa9ee92be15b
Lapatei <sashalapatej@gmail.com>
2024-10-20 12:46:34 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,27 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
         email: "",
         message: ""
     };
+
     const form = document.querySelector('.feedback-form');
     const emailInput = form.querySelector('input[name="email"]');
     const messageInput = form.querySelector('textarea[name="message"]');
+
     const saveToLocalStorage = () => {
         localStorage.setItem('feedback-form-state', JSON.stringify(formData));
     };
+
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        emailInput.value = email;
+        messageInput.value = message;
+        formData.email = email;
+        formData.message = message;
+    }
+
     form.addEventListener('input', event => {
         const { name, value } = event.target;
         formData[name] = value;
         saveToLocalStorage();
     });
-    document.addEventListener('DOMContentLoaded', () => {
-        const savedData = localStorage.getItem('feedback-form-state');
-        if (savedData) {
-            const { email, message } = JSON.parse(savedData);
-            emailInput.value = email;
-            messageInput.value = message;
-            formData.email = email;
-            formData.message = message;
-        }
-    });
+
     form.addEventListener('submit', event => {
         event.preventDefault();
         const { email, message } = formData;
